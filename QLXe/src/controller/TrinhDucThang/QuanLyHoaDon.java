83b93afa93855df6b7975f484f061478fb0a0b17
DucThang <139322881+thang7904@users.noreply.github.com>
2024-05-05 00:32:00 +0700

    Update QuanLyHoaDon.java

--- QLXe/src/controller/TrinhDucThang/QuanLyHoaDon.java
+++ QLXe/src/controller/TrinhDucThang/QuanLyHoaDon.java
@@ -3,5 +3,80 @@ package controller.TrinhDucThang;
 
 
 public class QuanLyHoaDon {
-    
+    private String maHD;
+    private String tenkhachhang;
+    private String diachi;
+    private String sodienthoai;
+    private String thoigian;
+    private String tongthanhtoan;
+
+    public HoaDon(String maHD, String tenkhachhang, String diachi, String sodienthoai, String thoigian, String tongthanhtoan) {
+        this.maHD = maHD;
+        this.tenkhachhang = tenkhachhang;
+        this.diachi = diachi;
+        this.sodienthoai = sodienthoai;
+        this.thoigian = thoigian;
+        this.tongthanhtoan = tongthanhtoan;
+    }
+
+    public String getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(String maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getTenkhachhang() {
+        return tenkhachhang;
+    }
+
+    public void setTenkhachhang(String tenkhachhang) {
+        this.tenkhachhang = tenkhachhang;
+    }
+
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
+    }
+
+    public String getSodienthoai() {
+        return sodienthoai;
+    }
+
+    public void setSodienthoai(String sodienthoai) {
+        this.sodienthoai = sodienthoai;
+    }
+
+    public String getThoigian() {
+        return thoigian;
+    }
+
+    public void setThoigian(String thoigian) {
+        this.thoigian = thoigian;
+    }
+
+    public String getTongthanhtoan() {
+        return tongthanhtoan;
+    }
+
+    public void setTongthanhtoan(String tongthanhtoan) {
+        this.tongthanhtoan = tongthanhtoan;
+    }
+
+    @Override
+    public String toString() {
+        return "HoaDon{" +
+                "maHD='" + maHD + '\'' +
+                ", tenkhachhang='" + tenkhachhang + '\'' +
+                ", diachi='" + diachi + '\'' +
+                ", sodienthoai='" + sodienthoai + '\'' +
+                ", thoigian='" + thoigian + '\'' +
+                ", tongthanhtoan='" + tongthanhtoan + '\'' +
+                '}';
+    }
 }
+
